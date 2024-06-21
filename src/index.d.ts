1b5c7ca3bd673381f9e6411c2503e337359a1e83
bathan1 <side_nathan@hotmail.com>
2024-06-21 12:17:11 -0700

    6

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,9 @@
-export default function redisAdapter(l2client: any): {
-    set_kv: (k: string, v: string) => Promise<0 | 1>;
-    get_key: (k: string) => Promise<string>;
-    del_key: (k: string) => Promise<0 | 1>;
-};
+import { createClient } from "redis";
+
+interface DecacheAdapter {
+  set_kv: (k: string, v: string) => Promise<number>;
+  get_key: (k: string) => Promise<string | null>;
+  del_key: (k: string) => Promise<number>;
+}
+
+export default function redisAdapter(l2client: ReturnType<typeof createClient>): DecacheAdapter;
