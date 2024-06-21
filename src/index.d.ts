d4acb5bc2298fad79ec3ddca48eb5218c51d0e7d
bathan1 <side_nathan@hotmail.com>
2024-06-21 12:26:06 -0700

    make ret type explicit

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,11 @@
 import { createClient } from "redis";
 
+type RedisClient = ReturnType<typeof createClient>;
+
 interface DecacheAdapter {
   set_kv: (k: string, v: string) => Promise<number>;
   get_key: (k: string) => Promise<string | null>;
   del_key: (k: string) => Promise<number>;
 }
 
-export default function redisAdapter(l2client: ReturnType<typeof createClient>): DecacheAdapter;
+export default function redisAdapter(l2client: RedisClient): DecacheAdapter;
